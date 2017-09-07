cbee23f1ad0611937cb2d166456c351c35d5bb13
Jussi Mattas <jussi.mattas@gmail.com>
2017-09-07 23:52:42 +0300

    Move constants to separate file.

---
+++ Constants.js
@@ -0,0 +1,13 @@
+const CANVAS_WIDTH = 1280;
+const CANVAS_HEIGHT = 720;
+
+const GRID_SIZE = 10;
+const PLAYER_W = 2*GRID_SIZE;
+const PLAYER_H = 2*GRID_SIZE;
+
+const Objects = {
+    NONE: 0,
+    PLAYER: 1,
+    GOAL: 2,
+    OBSTACLE: 3
+};
