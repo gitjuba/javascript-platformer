0fd67a7710c3a894ded0ef65f8a2810ce0bb076b
Jussi Mattas <jussi.mattas@gmail.com>
2019-07-18 15:14:07 +0300

    Deploy GH pages from docs folder.
    And rename npm scripts.

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   mode: isProd ? 'production' : 'development',
   entry: './src/index.js',
   output: {
-    path: __dirname + '/dist',
+    path: __dirname + '/docs',
     filename: 'main.js'
   },
   devtool: isProd ? '' : 'cheap-eval-source-map',
